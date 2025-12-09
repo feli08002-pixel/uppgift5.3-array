a63c578f843c671998accf083bd62dc549b0cb0f
feli08002 <feli08002@E-PW05SJ7G>
2025-12-09 21:54:36 +0100

    Add project files.

---
+++ uppgift5.3 array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System;
+
+namespace Uppgift5_3array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = new string[4];
+            string[] svar = new string[4];
+
+            frågor[0] = "Vad heter världens högsta berg?";
+            svar[0] = "1945";
+
+            frågor[1] = "Vilken är världens största ocean?";
+            svar[1] = "Sverige";
+
+            frågor[2] = "Vilken planets är närmast solen?";
+            svar[2] = "Stockholm";
+
+            frågor[3] = "Vem var första människan på månen?";
+            svar[3] = "1969";
+
+        }
+    }
+}
